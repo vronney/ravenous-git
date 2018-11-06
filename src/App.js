9f9b5d9a572bd2cb75e832b9ad13f23637f02030
Ronney Vargas <ronneyvargas@gmail.com>
2018-11-05 20:46:17 -0600

    added files for ravenous react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import BusinessList from './components/BusinessList/BusinessList.js';
+import SearchBar from './components/SearchBar/SearchBar.js';
+import Yelp from './util/Yelp.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      businesses: []
+    };
+    this.searchYelp = this.searchYelp.bind(this);
+  }
+  searchYelp(term, location, sortBy) {
+    Yelp.search(term, location, sortBy).then(businesses => {
+      this.setState({
+        businesses: businesses
+      });
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Ravenous</h1>
+        <SearchBar searchYelp={this.searchYelp} />
+        <BusinessList businesses={this.state.businesses} />
       </div>
     );
   }
---
+++ src/util/Yelp.js
@@ -0,0 +1,33 @@
+const apiKey = 'qkngjNASySDRtKdy9w3CZI9BizsvxAStskPZGW406ZHJn2zDN6JFhNVTU8srnGyRJ1Hg3PFaMWhU6Rz5Ohu8Cm27VB8TB6U4y-ATtlElx-66cAQQLtDXY2d_JP3VW3Yx';
+
+const Yelp = {
+    search(term, location, sortBy) {
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+                headers: {
+                    'Authorization': `Bearer ${apiKey}`
+                }
+         }).then(response => {
+            return response.json();
+         }).then(jsonResponse => {
+            if (jsonResponse.businesses) {
+                console.log(jsonResponse.businesses);
+                return jsonResponse.businesses.map(business => {
+                    return {
+                    id: business.id,
+                    imageSrc: business.image_url,
+                    name: business.name,
+                    address: business.location.address1,
+                    city: business.location.city,
+                    state: business.location.state,
+                    zipCode: business.location.zip_code,
+                    category: business.categories[0].title,
+                    rating: business.rating,
+                    reviewCount: business.review_count
+                    }
+                });
+            }
+        });
+    }
+};
+
+export default Yelp;
