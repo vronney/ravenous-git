e26748e83752f2b172c62ccb84a0c9da25f08d55
Ronney Vargas <ronneyvargas@gmail.com>
2018-11-05 20:56:53 -0600

    Updates

---
+++ precache-manifest.8aec10bfda4ce782e96117ed44711027.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9bec1e440c1c9807b57e",
+    "url": "/ravenous-git/static/js/runtime~main.9bec1e44.js"
+  },
+  {
+    "revision": "1bfb251b7d0ee55863a0",
+    "url": "/ravenous-git/static/js/main.1bfb251b.chunk.js"
+  },
+  {
+    "revision": "fd97677c7b9454c5e64f",
+    "url": "/ravenous-git/static/js/1.fd97677c.chunk.js"
+  },
+  {
+    "revision": "1bfb251b7d0ee55863a0",
+    "url": "/ravenous-git/static/css/main.f207763a.chunk.css"
+  },
+  {
+    "revision": "f8d23589a5f3565d41e19fc5a6f7df12",
+    "url": "/ravenous-git/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ravenous-git/precache-manifest.67564f4688d52b927ae172863ebc6c75.js"
+  "/ravenous-git/precache-manifest.8aec10bfda4ce782e96117ed44711027.js"
 );
 
 workbox.clientsClaim();
